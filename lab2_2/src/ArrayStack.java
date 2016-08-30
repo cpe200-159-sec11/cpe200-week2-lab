e827e0623fa3fc2b9aa7243c51df13683c3e0d6e
pattakorn_p <pattakorn_p@cmu.ac.th>
2016-08-30 15:45:14 +0700

    Add code

--- lab2_2/src/ArrayStack.java
+++ lab2_2/src/ArrayStack.java
@@ -20,6 +20,7 @@ public class ArrayStack {
 
     public void push(Object o)
     {
+        storage[++top] = o;
         /* Your code here */
     }
 
@@ -33,7 +34,7 @@ public class ArrayStack {
     public int size()
     {
         /* Your code here */
-        return 0;
+        return top+1;
     }
 
     public static void main(String[] args) {
