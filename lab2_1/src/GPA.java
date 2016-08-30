9cf5d252083d017cbec183599ec6d315fec332ba
Lenovo <poramin_s@cmu.ac.th>
2016-08-30 16:00:33 +0700

    GOOD

--- lab2_1/src/GPA.java
+++ lab2_1/src/GPA.java
@@ -2,14 +2,28 @@
  * Implement grading system.
  */
 public class GPA {
+
+    private float gpa = 0;
+            float count=0;
     public void addGrade(int credit, float grade) {
-        /* Your code here */
+
+        gpa = gpa + (credit*grade);
+        count = count + credit;
     }
 
 
     public float get() {
-        /* Your code here */
-        return 0.0f;
+
+        float sum;
+        if(count==0) {
+        sum =0;
+        }
+        else
+        {
+            sum = gpa/count;
+        }
+
+        return sum;
     }
     public static void main(String[] args) {
         GPA gpa = new GPA();
@@ -17,6 +31,6 @@ public class GPA {
         gpa.addGrade(3, 4);     // Got A for 3 credits class
         gpa.addGrade(3, 3.5f);   // Got B+ for 3 credits class
         gpa.addGrade(2, 2);     // Got C for a 2 credits class
-        System.out.printf("My GPS is %f\n", gpa.get());
+        System.out.printf("My GPS is %.2f\n", gpa.get());
     }
 }
