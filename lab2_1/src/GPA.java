777af0a22f9ba393a7a59933b020ff18917c8c79
Autapan <armindy01@gmail.com>
2016-08-30 16:37:13 +0700

    ADD

--- lab2_1/src/GPA.java
+++ lab2_1/src/GPA.java
@@ -2,17 +2,26 @@
  * Implement grading system.
  */
 public class GPA {
+
+    private int sumcredit=0;
+    private float sumgrade=0;
     public void addGrade(int credit, float grade) {
-        /* Your code here */
+        /* Your code here */ //array
+
+        sumgrade+=grade*credit;
+        sumcredit+=credit;
     }
 
 
     public float get() {
-        /* Your code here */
-        return 0.0f;
+        /* Your code here *///cal
+        if(sumgrade==0&&sumcredit==0){
+            return 0;
+        }
+        else return sumgrade/sumcredit;
     }
     public static void main(String[] args) {
-        GPA gpa = new GPA();
+        GPA gpa = new GPA(); //สร้างobject
         System.out.println("Add data");
         gpa.addGrade(3, 4);     // Got A for 3 credits class
         gpa.addGrade(3, 3.5f);   // Got B+ for 3 credits class
