8d0e043b4c05368f22a23f7696b0d1e68b4287b5
Tanakorn <dew4521@gmail.com>
2016-09-01 14:44:23 +0700

    add function code in GPA class

--- lab2_1/src/GPA.java
+++ lab2_1/src/GPA.java
@@ -2,14 +2,38 @@
  * Implement grading system.
  */
 public class GPA {
+    public int[] creditarr = new int[100];
+    public float[] gradearr = new float[100];
+    public int size=0;
+
     public void addGrade(int credit, float grade) {
+        creditarr[size] = credit;
+        gradearr[size] = grade;
+        size++;
         /* Your code here */
     }
 
 
     public float get() {
+        float sumgrade =0, sumcredit = 0, Gpa=0;
+
+        for(int i=0;i<size;i++) {
+            sumcredit = sumcredit + creditarr[i];
+            sumgrade = sumgrade + (gradearr[i]*creditarr[i]);
+        }
+        if(sumcredit==0)
+        {
+            Gpa =0;
+        }else
+        {
+            Gpa = sumgrade/sumcredit;
+        }
+
+        return Gpa;
+
+
+
         /* Your code here */
-        return 0.0f;
     }
     public static void main(String[] args) {
         GPA gpa = new GPA();
